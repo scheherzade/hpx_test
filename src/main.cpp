c8b306290645d1d1bfc9d7d753c3f1e3e3384ccd
Shahrzad Shirzad <shahrzadshirzad@gmail.com>
2018-12-02 21:39:44 -0600

    updating the previous code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-//#include <hpx/hpx_init.hpp>
-//#include <hpx/hpx_main.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/hpx.hpp>
 #include <hpx/include/iostreams.hpp>
@@ -8,37 +6,41 @@
 #include <hpx/include/parallel_executor_parameters.hpp>
 #include <numeric>
 
-//void print_array(std::vector<double> M)
-//{
-//	for (std::size_t i=0;i<M.size();i++) {
-//            std::cout << M[i] << "   ";
-//	}
-//}
-
 
 int hpx_main(boost::program_options::variables_map& vm)
 {
     using hpx::parallel::for_loop;
     using hpx::parallel::execution::par;
 
-    std::size_t n = vm["size"].as<std::size_t>();
+    std::size_t n = vm["vec_size"].as<std::size_t>();
     std::size_t chunk_size = vm["chunk_size"].as<std::size_t>();
-
+    bool static_scheduling=vm.count("static")!=0;
 
     std::vector<double> x(n,1);
     std::vector<double> y(n,1);
     double a=3.0;
 
-//    size_t size=16UL;
-//    size_t chunk_size=8UL;
-    hpx::parallel::execution::dynamic_chunk_size ds(chunk_size);
-    std::uint64_t t = hpx::util::high_resolution_clock::now();
-    for_loop( par.with(ds), size_t(0), n, [&](int i)
+    if (!static_scheduling)
     {
-        y[i] += a * x[i];
-    });
-    std::uint64_t elapsed = hpx::util::high_resolution_clock::now() - t;
-    std::cout<<"vector size:   "<<n<<"   chunk size: "<<chunk_size<<"   time(ns): "<<elapsed*1e-9<<std::endl;
+        hpx::parallel::execution::dynamic_chunk_size ds(chunk_size);
+        std::uint64_t t = hpx::util::high_resolution_clock::now();
+        for_loop( par.with(ds), size_t(0), n,[&](int i)
+        {
+            y[i] += a * x[i];
+        });
+        std::uint64_t elapsed = hpx::util::high_resolution_clock::now() - t;
+        std::cout<<"Dynamic scheduling: vector size:   "<<n<<"   chunk size: "<<chunk_size<<"   time(ns): "<<elapsed*1e-9<<std::endl;
+    }
+    else
+    {
+        hpx::parallel::execution::static_chunk_size s;
+        std::uint64_t t = hpx::util::high_resolution_clock::now();
+        for_loop(par.with(s), size_t(0), n, [&](int i) {
+            y[i] += a * x[i];
+        });
+        std::uint64_t elapsed = hpx::util::high_resolution_clock::now() - t;
+        std::cout << "Static scheduling: vector size:   " << n << "   time(ns): " << elapsed * 1e-9 << std::endl;
+    }
 
     double sum_of_elems = std::accumulate(y.begin(), y.end(), 0);
     HPX_TEST_EQ(sum_of_elems, 4*n);
@@ -52,8 +54,9 @@ int main(int argc, char* argv[])
 
     po::options_description desc_commandline;
     desc_commandline.add_options()
-            ("size", po::value<std::size_t>()->default_value(10000),"vector size")
+            ("vec_size", po::value<std::size_t>()->default_value(10000),"vector size")
             ("chunk_size", po::value<std::size_t>()->default_value(1),"chunk size")
+            ("static", "static scheduling")
             ;
 
     // Initialize and run HPX
